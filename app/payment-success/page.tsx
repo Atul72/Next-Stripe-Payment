126a47e6d9c848636a8b7944af5299ea0b65c8df
Atul72 <rai703537@gmail.com>
2024-11-01 16:41:11 +0530

    fix payment success error

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,11 +1,8 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-
-export default function PaymentSuccess() {
-  const searchParams = useSearchParams();
-  const amount = searchParams.get("amount");
-
+export default function PaymentSuccess({
+  searchParams: { amount },
+}: {
+  searchParams: { amount: string };
+}) {
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
       <div className="mb-10">
